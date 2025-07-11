7dacd75284dad0daab7c72c994a56a8a2e08c08a
Musa Rahim <musarahim13@gmail.com>
2025-07-11 12:43:12 +0300

    login and landing page design

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+     theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: '#1C597B',
+        },
+      },
+    },
+  },
+  
 };
 
 export default nextConfig;
---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,8 @@
+
+function page() {
+  return (
+    <div>Register from here</div>
+  )
+}
+
+export default page
